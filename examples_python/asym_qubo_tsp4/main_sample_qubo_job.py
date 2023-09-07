1281cbd49ded1d3e5b1672cf481090ff2e7ae726
Wesley Dyk <qci_github@quantumcomputinginc.com>
2023-09-07 12:11:19 -0600

    Added examples_python

---
+++ examples_python/asym_qubo_tsp4/main_sample_qubo_job.py
@@ -0,0 +1,32 @@
+import json
+from qci_client import QciClient
+from qci_client import load_json_file
+
+
+# 1) Get the client for token management
+qci = QciClient()
+
+# 2) Get the QUBO JSON data file
+json_data = load_json_file("qubo.json")
+
+# 3) POST the QUBO data file to the Files API, and get a file ID
+response_json = qci.upload_file(json_data)
+qubo_file_id = response_json["file_id"]
+
+# 4) Get the sample_qubo job template, and get a file ID
+job_json = load_json_file("sample_qubo_job_template.json")
+print(job_json)
+
+# 5) Edit the sample_qubo job template. Attach the QUBO data file to the QUBO
+# job.
+job_json["qubo_file_id"] = qubo_file_id
+
+# 6) Run the job
+job_submit_response_json = qci.process_job(job_body=job_json, job_type="sample-qubo")
+
+# 7) Obtain the job results - and check the energy, balance, and cut size
+if job_submit_response_json['job_info']['details']['status'] == "COMPLETED":
+    results = job_submit_response_json['results']
+    assert results['energies'][0] == -63251
+else:
+    print(job_submit_response_json['job_info']['results']['error'])
---
+++ examples_python/sample_hamiltonian_three/eqc2_continuous_main_sample_hamiltonian_job.py
@@ -0,0 +1,33 @@
+import json
+from qci_client import QciClient
+from qci_client import load_json_file
+
+
+# 1) Get the token obtainer
+qci = QciClient()
+
+# 1) Get the Hamiltonian data file
+json_data = load_json_file("hamiltonian.json")
+
+# 3) POST the Hamiltonian data file to the Files API
+response_json = qci.upload_file(json_data)
+hamiltonian_file_id = response_json["file_id"]
+
+# 4) Get the sample_hamiltonian job template
+job_json = load_json_file("sample_hamiltonian_job_template.json")
+
+# 5) Edit the sample_hamiltonian job template
+job_json["hamiltonian_file_id"] = hamiltonian_file_id
+params_vec = {}
+params_vec["sampler_type"] = "eqc2"
+params_vec["solution_type"] = "continuous"
+job_json["params"] = params_vec
+
+# 6) Submit the sample_hamiltonian job
+job_submit_response_json = qci.process_job(job_body=job_json, job_type="sample-hamiltonian")
+if job_submit_response_json['job_info']['details']['status'] == "COMPLETED":
+    results = job_submit_response_json['results']
+    print(results['energies'])
+    print(results['samples'])
+else:
+    print(job_submit_response_json['job_info']['results']['error'])
